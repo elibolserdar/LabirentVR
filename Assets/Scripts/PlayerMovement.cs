9326c41ca82c2ad4c2d84f432225fb685bf6a007
Serdar Elibol <elibol386@gmail.com>
2026-08-01 15:33:49 +0300

    Create experiment room layout

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,24 @@
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private float speed = 5f;
+    [SerializeField] private float speed = 5f;
 
-    void Update()
+    private CharacterController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
 
-        transform.Translate(direction * speed * Time.deltaTime);
+        controller.Move(move * speed * Time.deltaTime);
     }
 }
